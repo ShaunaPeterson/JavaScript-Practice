b3a59114e64059a9a6843a2688a1dbc801293891
shauna.s.peterson@hotmail.com <shauna.s.peterson@hotmail.com>
2021-07-06 21:48:16 -0400

    added sound

--- script.js
+++ script.js
@@ -10,10 +10,13 @@
 // }
 // document.addEventListener("keydown", logkey(event));
 
-// window.addEventListener('keydown', function(e) {
-//     const audio = document.querySelector(`audio[data-key${e.keyCode}]`);
-//     console.log(audio);
-// });
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if(!audio) return; //prevents keys from running if there is no audio attached
+    audio.play();
+
+});
 
 
 window.addEventListener('keydown', function (e) {
