c60b19320be4002ccffe2e1b22d1106e86d10c4f
shauna.s.peterson@hotmail.com <shauna.s.peterson@hotmail.com>
2021-07-06 21:30:58 -0400

    corrected javascript by adding parenthesis

--- script.js
+++ script.js
@@ -2,21 +2,25 @@
 // const key = document.querySelectorAll('.key');
 
 
-
+// document.getElementById('yeah').addEventListener("click", () => {
+//     console.log('yeah');
+// });
 // function logkey(event) {
 //     console.log(event);
 // }
 // document.addEventListener("keydown", logkey(event));
 
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key${e.keyCode}]`);
-    console.log(audio);
+// window.addEventListener('keydown', function(e) {
+//     const audio = document.querySelector(`audio[data-key${e.keyCode}]`);
+//     console.log(audio);
+// });
+
+
+window.addEventListener('keydown', function (e) {
+    console.log(e);
 });
 
 
 
 
-
-
-
-})
+})();
