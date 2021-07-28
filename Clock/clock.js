507b4fac48b7a61e4ab3a5c0201c937c4fd6ba4a
shauna.s.peterson@hotmail.com <shauna.s.peterson@hotmail.com>
2021-07-27 22:50:06 -0400

    Finished clock

--- Clock/clock.js
+++ Clock/clock.js
@@ -1,6 +1,25 @@
 (function(){
+
+let secondHand = document.querySelector('.second-hand');
+let hourHand = document.querySelector('.hour-hand');
+let minuteHand = document.querySelector('.min-hand');
+
     function setDate() {
-        console.log('Hi');
+        // console.log('Hi');
+        let rightNow = new Date();
+        let seconds = rightNow.getSeconds();
+        let minutes = rightNow.getMinutes();
+        let hour = rightNow.getHours();
+        let secondsDegrees = ((seconds / 60) * 360) + 90;
+        // console.log(seconds);
+        let hourDegree = ((hour / 12) * 360) + 90;
+        let minutesDegree = ((minutes / 60) * 360) + 90;
+
+        hourHand.style.transform = `rotate(${hourDegree}deg)`;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+        
+        
 
     }
 setInterval(setDate, 1000);
